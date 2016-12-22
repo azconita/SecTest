96425326ac0acf509747e914a84da79b83e28025
azconita <gabriela.azcona@despegar.com>
2016-12-22 13:50:59 -0300

    test zap plugin

---
+++ sectest.py
@@ -0,0 +1,78 @@
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+      render_template, flash
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'sectest.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('TEST_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+#@app.cli.command('initdb')
+#def initdb_command():
+#    """Initializes the database."""
+#    init_db()
+#    print('Initialized the database.')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('logged'))
+    return render_template('login.html', error=error)
+
+@app.route('/logged', methods=['GET'])
+def logged():
+    return render_template('logged.html', error=None)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', error=None)
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    return render_template('index.html', error=None)
+
+#if __name__ ==  "__main__":
+    #app.run()
