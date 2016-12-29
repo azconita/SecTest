797013fbacee269d90b6c2d1d41db9e77d28b228
azconita <gabriela.azcona@despegar.com>
2016-12-29 11:36:04 -0300

    Update sectest.py

--- sectest.py
+++ sectest.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from Flask.flask import Flask, request, session, g, redirect, url_for, abort, \
+from flask.flask import Flask, request, session, g, redirect, url_for, abort, \
       render_template, flash
 
 
